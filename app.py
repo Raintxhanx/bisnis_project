202dab411bf8335a0b832c3eb3e049e771fc0d6e
Raintxhanx <carisajkt23@gmail.com>
2025-07-07 21:31:33 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+from scipy.stats import yeojohnson
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
